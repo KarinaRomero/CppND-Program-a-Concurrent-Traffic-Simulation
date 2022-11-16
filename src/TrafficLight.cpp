60f38a8242a1e6d8e73ca9bebdf63ea25f4327c1
Karina Romero <betzy_skamm@hotmail.com>
2022-11-16 09:50:57 -0600

    add FP.5 MessageQueue::receive

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -12,6 +12,20 @@ T MessageQueue<T>::receive()
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+
+    // Lock the _queue
+    std::unique_lock<std::mutex> lock(_mutex);
+    // Apply wait condition if the queue isn't empty
+    
+    _condition.wait(lock, [this] { return !_queue.empty();});
+
+    // Get the last element in the queue
+    auto msg = std::move(_queue.back());
+    // Remove the last element in the queue
+    _queue.pop_back();
+
+    // Return the element
+    return msg;
 }
 
 template <typename T>
@@ -19,6 +33,13 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    
+    // Lock the _queue
+    std::lock_guard<std::mutex> lock(_mutex);
+    // Add message to the queue
+    _queue.push_back(std::move(msg));
+    // Notify that the add operation has already been performed
+    _condition.notify_one();
 }
 
 
@@ -35,6 +56,15 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    
+    // Add infinite while-loop 
+    while (true)
+    {
+        // Get the message and check if its green light
+        if(_mesageQueue.receive() == TrafficLightPhase::green)
+            return;
+    }
+    
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -80,6 +110,9 @@ void TrafficLight::cycleThroughPhases()
             // Change the color of the light to green
             _currentPhase = TrafficLightPhase::green;
         }
+
+        // Send current TrafficLightPhase state
+        _mesageQueue.send(std::move(_currentPhase));
 
         // Update values for the next loop
         cycleDuration = rand() % 4 - 6;
